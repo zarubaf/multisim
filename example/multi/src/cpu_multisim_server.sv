a1eccd7aadc2742156cac18ff8cdb787009f2c19
antoinemadec <ajamadec@gmail.com>
2025-06-24 20:27:47 +0200

    feat: remove extra dummy channels for cleaner code

--- example/multi/src/cpu_multisim_server.sv
+++ example/multi/src/cpu_multisim_server.sv
@@ -1,7 +1,4 @@
-module cpu_multisim_server #(
-    // generate dummy client/server channels to see impact on performance
-    parameter int EXTRA_DUMMY_CHANNELS = 0
-) (
+module cpu_multisim_server (
     input bit clk,
     input bit [31:0] cpu_index,
     // cpu -> noc
@@ -44,44 +41,5 @@ module cpu_multisim_server #(
       .data_vld   (data_noc_to_cpu_vld),
       .data       (data_noc_to_cpu)
   );
-
-  // generate dummy client/server channels to see impact on performance
-  for (
-      genvar dummy_channel_idx = 0; dummy_channel_idx < EXTRA_DUMMY_CHANNELS; dummy_channel_idx++
-  ) begin : gen_dummy_channels
-    string server_name_cpu_to_noc_dummy;
-    string server_name_noc_to_cpu_dummy;
-    initial begin
-      wait (server_name_cpu_to_noc != "");
-      wait (server_name_noc_to_cpu != "");
-      $sformat(server_name_cpu_to_noc_dummy, "%0s_dummy_channel_%0d", server_name_cpu_to_noc,
-               dummy_channel_idx);
-      $sformat(server_name_noc_to_cpu_dummy, "%0s_dummy_channel_%0d", server_name_noc_to_cpu,
-               dummy_channel_idx);
-    end
-
-    bit dummy_cpu_to_noc_vld;
-    bit [63:0] dummy_cpu_to_noc;
-
-    multisim_server_pull #(
-        .DATA_WIDTH(64)
-    ) i_multisim_server_pull (
-        .clk        (clk),
-        .server_name(server_name_cpu_to_noc_dummy),
-        .data_rdy   (1),
-        .data_vld   (dummy_cpu_to_noc_vld),
-        .data       (dummy_cpu_to_noc)
-    );
-
-    multisim_server_push #(
-        .DATA_WIDTH(64)
-    ) i_multisim_server_push (
-        .clk        (clk),
-        .server_name(server_name_noc_to_cpu_dummy),
-        .data_rdy   (  /*unused*/),
-        .data_vld   (1),
-        .data       (64'hcafedeca_deadbeef)
-    );
-  end
 
 endmodule
